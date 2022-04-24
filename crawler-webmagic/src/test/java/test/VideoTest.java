8fe713707de1be6b94f2550614348b70f13980c7
xtayicy <xtayicy@gmail.com>
2022-04-24 08:58:06 +0800

    video

--- crawler-webmagic/src/test/java/test/VideoTest.java
+++ crawler-webmagic/src/test/java/test/VideoTest.java
@@ -23,26 +23,41 @@ public class VideoTest {
 		Elements moreEelements = doc.select(".more");
 		for (Element moreElement : moreEelements) {
 			String url = moreElement.select("a").attr("href");
-			if(size == 1){
+			//if(size == 1){
 				doc = Jsoup.connect(url).get();
-				Element postElement = doc.select("#posts").get(0);
+				/*Element postElement = doc.select("#posts").get(0);
 				Elements postEelements = postElement.select(".post");
 				for (Element e1 : postEelements) {
 					url = e1.select("a").attr("href");
 					//System.out.println(url);
-				}
+				}*/
 				
 				Element paginationElement = doc.select(".pagination").get(0);
-				System.out.println(paginationElement);
+				//System.out.println(paginationElement);
 				
-				Element nextPageElement = paginationElement.select(".next-page > a").get(0);
-				String nextPageUrl = nextPageElement.select("a").attr("href");
-				System.out.println(nextPageUrl);
+				for (Element liElement : paginationElement.children().get(0).children()) {
+					Element nextElementSibling = liElement.nextElementSibling();
+					if(nextElementSibling != null && nextElementSibling.text().equals("下一页")){
+						url = liElement.select("a").attr("href");
+						System.out.println(url);
+						String preUrl = url.substring(0,url.lastIndexOf("/") + 1);
+						for (int i = Integer.parseInt(liElement.text()); i > 0 ; i--) {
+							url = preUrl + String.valueOf(i);
+							doc = Jsoup.connect(url).get();
+							Element postElement = doc.select("#posts").get(0);
+							Elements postEelements = postElement.select(".post");
+							for (Element e1 : postEelements) {
+								url = e1.select("a").attr("href");
+								System.out.println(url);
+							}
+						}
+					}
+				}
 				
-				size++;
-			}else{
+				//size++;
+			/*}else if(size == 2){
 				//System.out.println(url);
-			}
+			}*/
 		}
 	}
 }
